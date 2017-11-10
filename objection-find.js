b74c4f500aa06b8bdbf7503b953ce6c24a0ef021
AFM Sayem <afm.sayem@gmail.com>
2017-11-10 18:08:16 +0800

    chore(es6): port to es6

--- objection-find.js
+++ objection-find.js
@@ -1,13 +1,10 @@
 'use strict';
 
-var FindQueryBuilder = require('./lib/FindQueryBuilder');
-var QueryParameter = require('./lib/QueryParameter');
-var PropertyRef = require('./lib/PropertyRef');
+const FindQueryBuilder = require('./lib/FindQueryBuilder');
+const QueryParameter = require('./lib/QueryParameter');
+const PropertyRef = require('./lib/PropertyRef');
 
-module.exports = function (modelClass) {
-  return new FindQueryBuilder(modelClass);
-};
-
+module.exports = (modelClass) => new FindQueryBuilder(modelClass); 
 module.exports.FindQueryBuilder = FindQueryBuilder;
 module.exports.QueryParameter = QueryParameter;
 module.exports.PropertyRef = PropertyRef;
