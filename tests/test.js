06b8cefcccbe678b140df2a2faa66df31e754e54
David Payne <david@woodscamp.com>
2020-03-11 14:42:20 -0300

    Fix linting error in unit test

--- tests/test.js
+++ tests/test.js
@@ -105,7 +105,7 @@ describe('integration tests', () => {
                   'F09'
                 ]);
               });
-          })
+          });
         });
 
         describe('lt', function() {
